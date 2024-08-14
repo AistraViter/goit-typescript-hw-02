88f98509ffb5b05c2aad4949d791d8ea5ce4c2af
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-14 20:30:14 +0300

    T: gallery-api

--- src/gallery-api.ts
+++ src/gallery-api.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
+import { Image as ImageData } from "./types/ImageTypes";
 
 const API_KEY = "FMIJ5k-c5lppvR6WpV4KaIibT-R7gUAfjxBmMzlKrlI";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-export async function fetchImages(query, page) {
-  const { data } = await axios.get("/search/photos", {
+interface FetchImagesResponse {
+  total: number;
+  total_pages: number;
+  results: ImageData[];
+}
+
+export async function fetchImages(
+  query: string,
+  page: number
+): Promise<FetchImagesResponse> {
+  const { data } = await axios.get<FetchImagesResponse>("/search/photos", {
     params: {
       client_id: API_KEY,
       page: page,
