ffd1efb7eae7a73dd7490443c9e8ee0dc2a2f0fc
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-14 19:28:32 +0300

    T ImageCard

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,25 +1,46 @@
-import styles from "./ImageCard.module.css";
-const { imageCard } = styles;
-const ImageCard = ({ image, onClick }) => {
-  let orientation;
+import css from "./ImageCard.module.css";
 
-  if (
-    Math.abs(image.width - image.height) <
-    0.1 * Math.max(image.width, image.height)
-  ) {
-    orientation = "squarish";
-  } else if (image.width > image.height) {
-    orientation = "landscape";
-  } else {
-    orientation = "portrait";
+interface Image {
+  urls: {
+    small: string;
+  };
+  alt_description?: string;
+  width: number | null;
+  height: number | null;
+}
+
+interface ImageCardProps {
+  image: Image;
+  onClick: (event: React.MouseEvent<HTMLImageElement>) => void;
+}
+const ImageCard: React.FC<ImageCardProps> = ({ image, onClick }) => {
+  const imageUrl: string = image.urls.small;
+  let orientation: string | null = null;
+
+  if (image.width !== null && image.height !== null) {
+    if (
+      Math.abs(image.width - image.height) <
+      0.1 * Math.max(image.width, image.height)
+    ) {
+      orientation = "squarish";
+    } else if (image.width > image.height) {
+      orientation = "landscape";
+    } else {
+      orientation = "portrait";
+    }
   }
   return (
-    <div className={imageCard} onClick={onClick}>
-      <img src={image.urls.small} alt={image.alt_description} />
+    <div className={css.imageCard} onClick={onClick}>
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt={image.alt_description || "Image without description"}
+        />
+      ) : (
+        <span>No image available</span>
+      )}
       <span>{orientation}</span>
     </div>
   );
 };
 export default ImageCard;
-
-
